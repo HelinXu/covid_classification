f9e2ac06ec3f88b69312bf11a018624784a8bc1d
HelinXu <xhl19@mails.tsinghua.edu.cn>
2021-11-27 00:43:28 +0800

    dataset

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,11 @@ import os
 import random
 import pickle
 import logging
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torchvision.io import read_image
 
 logging.basicConfig(
                 level=logging.DEBUG,
@@ -36,3 +41,25 @@ def split_dataset(dataset_path='./data/', train_ratio=0.8):
     pickle.dump(train_data, open(os.path.join(dataset_path, 'train.pkl'), 'wb'))
     pickle.dump(test_data, open(os.path.join(dataset_path, 'test.pkl'), 'wb'))
     logging.info(f'Done split dataset. train {len(train_data)} = {len(train_covid_fn)} + {len(train_noncovid_fn)}; test {len(test_data)} = {len(test_covid_fn)} + {len(test_noncovid_fn)}')
+
+
+class CovidDataset(Dataset):
+    def __init__(self, dataset_fn='train.pkl', dataset_path='./data/', shuffle=True):
+        self.imgs = pickle.load(open(os.path.join(dataset_path, dataset_fn), 'rb'))
+        self.dataset_path = dataset_path
+        if shuffle: random.shuffle(self.imgs)
+
+    def __len__(self):
+        return len(self.imgs)
+    
+    def __getitem__(self, idx):
+        if self.imgs[idx][1]: img_path = os.path.join(self.dataset_path, 'COVID', self.imgs[idx][0])
+        else: img_path = os.path.join(self.dataset_path, 'NonCOVID', self.imgs[idx][0])
+        image = read_image(img_path)
+        # TODO: transform
+        return image, self.imgs[idx][1]
+
+        
+
+
+# print(CovidDataset().__getitem__(35))
