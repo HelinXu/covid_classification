57b0bf5858d732a32fbe6180e3396f2c40292d90
HelinXu <xhl19@mails.tsinghua.edu.cn>
2021-12-06 18:48:18 +0800

    update train.sh

--- train_aug.sh
+++ train_aug.sh
@@ -1,8 +1,8 @@
 python main.py --train \
         --use_data_augment \
-        --pretrain ./checkpoint/model_no_aug.pth \
-        --check_path ./checkpoint/model_no_aug.pth \
-        --epoch 500 \
-        --lr 1e-5 \
-        --writer_path ./tensorboard/no_aug
+        --pretrain ./checkpoint/model_aug.pth \
+        --check_path ./checkpoint/model_aug.pth \
+        --epoch 1000 \
+        --lr 3e-6 \
+        --writer_path ./tensorboard/aug
             
